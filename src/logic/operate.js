35190229a1f83bfe14838432705b28a7e9575ad9
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-10-29 18:32:03 +0300

    Refactor to use best practice of already existing modifiers in the Big package to reduce chance of bugs in logic

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -5,13 +5,13 @@ const operate = (numberOne, numberTwo, operation) => {
   const num1 = new Big(numberOne);
   const num2 = new Big(numberTwo);
   if (operation === '-') {
-    total = num1 - num2;
+    total = num1.sub(num2);
   } else if (operation === '+') {
-    total = num1 + num2;
+    total = num1.add(num2);
   } else if (operation === '*') {
-    total = num1 * num2;
+    total = num1.times(num2);
   } else {
-    total = num1 / num2;
+    total = num1.div(num2);
   }
   return total;
 };
