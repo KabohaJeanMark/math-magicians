6be5f23e0020f4c871f68b1900d8ae32f36eff1e
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-10-29 16:38:58 +0300

    Use ES6 arrow function syntax

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,6 +1,6 @@
 import operate from './operate';
 
-function calculate(calculatorDataObject, buttonName) {
+const calculate = (calculatorDataObject, buttonName) => {
   let { total, next, operation } = calculatorDataObject;
 
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
@@ -8,9 +8,9 @@ function calculate(calculatorDataObject, buttonName) {
   const operators = ['+', '-', 'X', '/'];
 
   if (buttonName === 'AC') {
-    total = ' ';
-    next = ' ';
-    operation = ' ';
+    total = '';
+    next = '';
+    operation = '';
   } else if (numbers.includes(buttonName) && operation === '') {
     total += buttonName;
   } else if (operators.includes(buttonName)) {
@@ -27,6 +27,6 @@ function calculate(calculatorDataObject, buttonName) {
     operation = '';
   }
   return { total, next, operation };
-}
+};
 
 export default calculate;
