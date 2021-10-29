b2698c8742be5a9b85289cdb147299deade86f93
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-10-29 16:36:23 +0300

    Work on single button operations and import operate to deal with operations on 2 numbers

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,9 +1,32 @@
+import operate from './operate';
+
 function calculate(calculatorDataObject, buttonName) {
-  let {total, next, operation} = calculatorDataObject;
+  let { total, next, operation } = calculatorDataObject;
 
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 
   const operators = ['+', '-', 'X', '/'];
+
+  if (buttonName === 'AC') {
+    total = ' ';
+    next = ' ';
+    operation = ' ';
+  } else if (numbers.includes(buttonName) && operation === '') {
+    total += buttonName;
+  } else if (operators.includes(buttonName)) {
+    operation = buttonName;
+    next = total;
+    total = '';
+  } else if (buttonName === '+/-') {
+    total *= -1;
+  } else if (buttonName === '%') {
+    total /= 100;
+  } else if (buttonName === '=') {
+    total = operate(total, next, operation);
+    next = '';
+    operation = '';
+  }
+  return { total, next, operation };
 }
 
 export default calculate;
