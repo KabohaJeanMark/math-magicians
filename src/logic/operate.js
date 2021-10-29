0fdead084b66d20d7b269be82c8e2662b8d7cf83
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-10-29 16:04:05 +0300

    Use ES6 arrow functions with operate method

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,6 +1,6 @@
 import Big from 'big.js';
 
-function operate(numberOne, numberTwo, operation) {
+const operate = (numberOne, numberTwo, operation) => {
   let total;
   const num1 = new Big(numberOne);
   const num2 = new Big(numberTwo);
@@ -14,6 +14,6 @@ function operate(numberOne, numberTwo, operation) {
     total = num1 / num2;
   }
   return total;
-}
+};
 
 export default operate;
