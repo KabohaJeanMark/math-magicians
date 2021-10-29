2c8c784a21c5fc012ce7ffc9d2d99b4cea0b69f5
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-10-29 16:11:22 +0300

    Destructure calculatorDataObject and create arrays for numbers and operators

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,5 +1,9 @@
-function calculate({ total, next, operation }, button) {
+function calculate(calculatorDataObject, buttonName) {
+  let {total, next, operation} = calculatorDataObject;
 
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+
+  const operators = ['+', '-', 'X', '/'];
 }
 
 export default calculate;
