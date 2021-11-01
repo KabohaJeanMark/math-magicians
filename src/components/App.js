8398e9b88f5d4806a3bf71d9c3df5761f9cf543e
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-01 13:10:55 +0300

    Refactor App from a functional to a class component and add default state

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,24 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-const App = () => (
-  <>
-    <Display result="0" />
-    <ButtonPanel />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Display result="0" />
+        <ButtonPanel />
+      </>
+    );
+  }
+}
 
 export default App;
