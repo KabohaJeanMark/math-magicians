de2d7ddfdcf9055a33980d43b9be1a2c7c509b67
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-09 17:25:47 +0300

    Add a quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,8 @@
+const Quote = () => (
+  <div>
+    <p>Human problems arise from man not being able to sit alone in a room and be quite</p>
+    <p> Blaise Pascal </p>
+  </div>
+);
+
+export default Quote;
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Home from './Home';
+import Quote from './Quote';
 
 const RouteSwitch = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} exact />
       <Route path="/Home" element={<Home />} />
+      <Route path="/Quote" element={<Quote />} />
     </Routes>
   </BrowserRouter>
 );
