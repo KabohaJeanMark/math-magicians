4cec55cee3424fa6ab32d6daecd9bb520ff7c16d
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-09 13:26:40 +0300

    Style button groups and display them horizontally in rows

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,31 +4,31 @@ import '../styles/ButtonPanel.css';
 
 const ButtonPanel = ({ clickHandler }) => (
   <div className="ButtonPanel">
-    <div>
+    <div className="buttonRow">
       <Button buttonClickMethod={clickHandler} name="AC" />
       <Button buttonClickMethod={clickHandler} name="+/-" />
       <Button buttonClickMethod={clickHandler} name="%" />
       <Button buttonClickMethod={clickHandler} name="÷" />
     </div>
-    <div>
+    <div className="buttonRow">
       <Button buttonClickMethod={clickHandler} name="7" />
       <Button buttonClickMethod={clickHandler} name="8" />
       <Button buttonClickMethod={clickHandler} name="9" />
       <Button buttonClickMethod={clickHandler} name="X" />
     </div>
-    <div>
+    <div className="buttonRow">
       <Button buttonClickMethod={clickHandler} name="4" />
       <Button buttonClickMethod={clickHandler} name="5" />
       <Button buttonClickMethod={clickHandler} name="6" />
       <Button buttonClickMethod={clickHandler} name="-" />
     </div>
-    <div>
+    <div className="buttonRow">
       <Button buttonClickMethod={clickHandler} name="1" />
       <Button buttonClickMethod={clickHandler} name="2" />
       <Button buttonClickMethod={clickHandler} name="3" />
       <Button buttonClickMethod={clickHandler} name="+" />
     </div>
-    <div>
+    <div className="buttonRow">
       <Button buttonClickMethod={clickHandler} name="0" />
       <Button buttonClickMethod={clickHandler} name="." />
       <Button buttonClickMethod={clickHandler} name="=" />
