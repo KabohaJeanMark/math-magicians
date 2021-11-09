9f390d6a754caba42ff5b6f2217398893c5de0ea
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-09 16:53:11 +0300

    Add 2 props and pass them for determining colour and width of button elements

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,33 @@
 import PropTypes from 'prop-types';
 import '../styles/Button.css';
 
-const Button = ({ name, buttonClickMethod }) => (
-  <div className="Button">
-    <button className="eachButton" onClick={() => buttonClickMethod(name)} type="button">{name}</button>
+const Button = ({
+  name, buttonClickMethod, color, wide,
+}) => (
+  <div
+    className="Button"
+    style={{
+      width: wide || '25%',
+    }}
+  >
+    <button
+      style={{
+        backgroundColor: color || 'lightgrey',
+      }}
+      className="eachButton"
+      onClick={() => buttonClickMethod(name)}
+      type="button"
+    >
+      {name}
+    </button>
   </div>
 );
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   buttonClickMethod: PropTypes.func.isRequired,
+  color: PropTypes.string.isRequired,
+  wide: PropTypes.string.isRequired,
 };
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,34 +4,34 @@ import '../styles/ButtonPanel.css';
 
 const ButtonPanel = ({ clickHandler }) => (
   <div className="ButtonPanel">
-    <div className="buttonRow">
-      <Button className="eachButton" buttonClickMethod={clickHandler} name="AC" />
-      <Button className="eachButton" buttonClickMethod={clickHandler} name="+/-" />
-      <Button className="eachButton" buttonClickMethod={clickHandler} name="%" />
-      <Button className="eachButton" buttonClickMethod={clickHandler} name="÷" />
+    <div className="buttonRow sideRow">
+      <Button buttonClickMethod={clickHandler} name="AC" />
+      <Button buttonClickMethod={clickHandler} name="+/-" />
+      <Button buttonClickMethod={clickHandler} name="%" />
+      <Button buttonClickMethod={clickHandler} name="÷" color="orange" />
     </div>
     <div className="buttonRow">
-      <Button className="eachButton" buttonClickMethod={clickHandler} name="7" />
-      <Button className="eachButton" buttonClickMethod={clickHandler} name="8" />
-      <Button className="eachButton" buttonClickMethod={clickHandler} name="9" />
-      <Button className="eachButton" buttonClickMethod={clickHandler} name="X" />
+      <Button buttonClickMethod={clickHandler} name="7" />
+      <Button buttonClickMethod={clickHandler} name="8" />
+      <Button buttonClickMethod={clickHandler} name="9" />
+      <Button buttonClickMethod={clickHandler} name="X" color="orange" />
     </div>
     <div className="buttonRow">
       <Button buttonClickMethod={clickHandler} name="4" />
       <Button buttonClickMethod={clickHandler} name="5" />
       <Button buttonClickMethod={clickHandler} name="6" />
-      <Button buttonClickMethod={clickHandler} name="-" />
+      <Button buttonClickMethod={clickHandler} name="-" color="orange" />
     </div>
     <div className="buttonRow">
       <Button buttonClickMethod={clickHandler} name="1" />
       <Button buttonClickMethod={clickHandler} name="2" />
       <Button buttonClickMethod={clickHandler} name="3" />
-      <Button buttonClickMethod={clickHandler} name="+" />
+      <Button buttonClickMethod={clickHandler} name="+" color="orange" />
     </div>
     <div className="buttonRow lastRow">
-      <Button buttonClickMethod={clickHandler} name="0" />
+      <Button buttonClickMethod={clickHandler} name="0" wide="50%" />
       <Button buttonClickMethod={clickHandler} name="." />
-      <Button buttonClickMethod={clickHandler} name="=" />
+      <Button buttonClickMethod={clickHandler} name="=" color="orange" />
     </div>
   </div>
 );
