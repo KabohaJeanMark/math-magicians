be83345f09bbb0876a23cbba5ad585766c9b94ac
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-11 07:46:35 +0300

    Add test for route path for the Quote Page

--- src/components/Routes.test.js
+++ src/components/Routes.test.js
@@ -1,11 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Routes from './Routes';
 
-describe('Routes renders all the 3 NavBar links', () => {
-  render(<Routes />);
-
+describe('Routes renders the NavBar links', () => {
   it('contains a link to the Calculator page', () => {
+    render(<Routes />);
     const navLink = screen.getByText(/Calculate/i);
+    expect(navLink).toBeInTheDocument();
+  });
+
+  it('contains a link to the Quote page', () => {
+    render(<Routes />);
+    const navLink = screen.getByText(/Quote/i);
     expect(navLink).toBeInTheDocument();
   });
 });
