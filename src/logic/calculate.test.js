786b49c91146eb6a2d86a46fc2846045a8d1fdf9
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-11 16:03:20 +0300

    Add both positive and negative tests for different scenarios

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,8 +1,21 @@
 /* eslint-disable no-unused-expressions */
 import Calculate from './calculate';
 
-test('should clear display if AC is clicked', () => {
-  const result = Calculate({ total: 10, next: 2, operation: '-' }, 'AC');
-  expect(result).toHaveProperty('total');
-  expect(result.total).toBe('');
+describe('tests for the calculate logic', () => {
+  it('should clear display if AC is clicked', () => {
+    const result = Calculate({ total: 10, next: 2, operation: '-' }, 'AC');
+    expect(result).toHaveProperty('total');
+    expect(result.total).toBe('');
+  });
+
+  it('should calculate the difference if subtract is clicked', () => {
+    const result = Calculate({ total: 4, next: 5, operation: '-' }, '=');
+    expect(result).toHaveProperty('total');
+    expect(result.total).toBe('1');
+  });
+
+  test('should NOT give a wrong result when multiplying', () => {
+    const result = Calculate({ total: 18, next: 6, operation: 'x' }, '=');
+    expect(result.total === 1000).toBeFalsy();
+  });
 });
